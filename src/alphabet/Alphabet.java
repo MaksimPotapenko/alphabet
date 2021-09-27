a8f1e90831aadbdaf9faab579e5c40e442493330
Max <maks.potapenko@gmail.com>
2021-09-27 20:47:41 +0300

    done

---
+++ src/alphabet/Alphabet.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alphabet;
+
+
+
+/**
+ *
+ * @author Mad Max
+ */
+public class Alphabet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String s = "Sphinx of black quartz, judge my vow";
+    System.out.println("Текст: " + s);
+    System.out.println("");
+    s = s.replaceAll("[^a-zA-Z]", "");
+    s = s.toLowerCase();        
+    s = s.replaceAll("(.)(?=.*\\1)", "");
+    System.out.println(s);
+    System.out.println("");
+    System.out.println("текст использует " + s.length() + " букв");
+    System.out.println("в тексте все буквы английского алфавита от a до z");
+
+}
+    
+}
